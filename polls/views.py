54778354553a3c07bd1788fd1a969d87d27988c9
sanyam188 <sanyamjain.188@gmail.com>
2020-03-27 11:47:09 +0530

    details page template added

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
@@ -23,8 +23,9 @@ def index22(request):
     return HttpResponse("Hello, Sanyam Sanyam")
 
 def detail(request, q_id):
-    print( q_id)
-    return HttpResponse("You are looking   at ques%s " %q_id)
+    # return render(request,'polls/details.html',{'question':Question.objects.get(pk=q_id)})
+    return render(request,'polls/details.html',{'question':get_object_or_404(Question, pk=q_id)})
+    # return HttpResponse("You are looking   at ques%s " %q_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
