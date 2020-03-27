86b98fa7e89f5549e25790067c21dd3197e5813b
sanyam188 <sanyamjain.188@gmail.com>
2020-03-27 09:44:32 +0530

    extra urls added

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('sj/', views.index2, name='index2'),
     path('sj/sj', views.index22, name='index2'),
+    path('<int:q_id>/', views.detail),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:2]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def index2(request):
@@ -12,3 +15,17 @@ def index2(request):
 
 def index22(request):
     return HttpResponse("Hello, Sanyam Sanyam")
+
+def detail(request, q_id):
+    print( q_id)
+    return HttpResponse("You are looking   at ques%s " %q_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+    
