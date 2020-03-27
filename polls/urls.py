3b2145ec603eb877148a57a176aa4a2ef8329d7a
sanyam188 <sanyamjain.188@gmail.com>
2020-03-27 12:06:48 +0530

    URL namespace created

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from . import views
-
+app_name='polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('sj/', views.index2, name='index2'),
     path('sj/sj', views.index22, name='index2'),
-    path('<int:q_id>/', views.detail),
+    path('<int:q_id>/', views.detail,name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
