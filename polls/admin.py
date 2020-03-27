8a5809361bf75fd33dd85fd65cb0391b04fc0224
sanyam188 <sanyamjain.188@gmail.com>
2020-03-27 16:02:52 +0530

    Static files added

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,14 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+# admin.site.register(Question)
 admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
